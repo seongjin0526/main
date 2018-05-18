2b8f72157327083da9c186c9a8e48ba1461e0f92
ahn <93immm@naver.com>
2018-05-18 10:32:44 +0900

    fixed untracked files

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 urlpatterns = [
-    path('', views.index ),
+    path('', views.index, name='index'),
+    url('default$', views.default, name='default'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -2,3 +2,26 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'frontend/index.html')
+
+
+def default(request):
+
+    """
+    making logic
+    """
+
+    """
+    with connections['default'].cursor() as cur:
+        query = '''
+            select *
+            FROM table
+            where sample = '{page}'
+        '''.format(page=page)
+        cur.execute(query)
+        rows = cur.fetchall()
+    """
+
+    context = {}
+    #context['sample_key'] = 'hello world'
+
+    return render(request, 'frontend/default.html', context)
